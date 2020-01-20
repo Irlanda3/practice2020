49e5dd09ae100bee0ae3527ac49fe4321021a76e
anayeli ochoa <ochoa@example.com>
2020-01-20 11:24:21 -0700

    using github

---
+++ learningGitHub.py
@@ -0,0 +1,8 @@
+'''github
+1 baja git
+2 https://git-scm.com/downloads
+3  intalalo y abre otra vez visual studio code
+4 fui a mi github e hice un repositorio nuevo luego copie el link y lo puse
+en el control shift p  pero le puse git clone y dentro de ese repositorio abri learningGitHub.py
+'''
+print("hola")
