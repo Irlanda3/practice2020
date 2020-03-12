e2cbe9120a380174e9dddabf443c393c628b9053
anayeli ochoa <ochoa@example.com>
2020-03-11 23:22:06 -0600

    details

--- learningGitHub.py
+++ learningGitHub.py
@@ -4,6 +4,9 @@
 3  intalalo y abre otra vez visual studio code
 4 fui a mi github e hice un repositorio nuevo luego copie el link y lo puse
 en el control + shift + p  pero le puse git clone y dentro de ese repositorio abri learningGitHub.py
+
+5 tengo droble folder como python->prectice2020 por que el github folder se
+llama practice2020 y yo ya tenia python folder. Es mejor no crear tanto folder.
 '''
 print("hola")
 print("conejito jjjjjjjjjjjjjjjjjjjjjjj")
