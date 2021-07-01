bea4c217b8d80be3ce612518717979233ff430c4
Chiki OR <chikior@Chikis-MacBook-Pro.local>
2021-07-01 16:18:47 -0600

    Django inside practice2020

---
+++ TechWithTim.py
@@ -0,0 +1,14 @@
+print("Hello", 'end', 87, False, end='\n') # end means go tot he next line after this one is printed
+
+# Variables
+hello = 'tim'
+world = 'world'
+print(hello, world)
+
+# underscores are possbile with python
+# hello_wolrd99 possible
+# 45hello not possible
+
+# Input
+name = input("Name: ")
+print(name)
---
+++ june21.py
@@ -0,0 +1,19 @@
+##for i in range(10):
+    print(i)
+    
+for x in range(6):
+  print(x)
+  #####
+  pip3 install pipenv
+  update python witn website download and then used pip not conda
+  no hice mi carpeta en Desktop, la ihce en Documents/Practice2020
+  mkdir storefront
+  cd storefront
+  pipenv istall django
+  Virtualenv location: /Users/chikior/.local/share/virtualenvs/storefront-1YGrGP2g
+  To activate this project's virtualenv, run pipenv shell.
+Alternatively, run a command inside the virtualenv with pipenv run.
+NOW DJANGO IS INSIDE THIS ENVRONMENT
+
+Activate teh environmnet: pipenv shell
+To start a new project: django-admin startproject storefront
