a108e116d115943f0151a038aa4abcff08c8a685
aochoa1@msudenver.edu <aocho1@msudenver.edu>
2022-02-25 17:34:30 -0600

    notes feb 3

--- Hello.py
+++ Hello.py
@@ -5,3 +5,5 @@ print('Hello World')
 
 #  git config --global user.email "you@example.com"
 # git config --global user.name "Your Name"
+#git commit -m "message"
+#git push
---
+++ classMethodsAndStaticMethods.py
@@ -0,0 +1,9 @@
+# python OOP
+class Employee:
+    
+    num_of_emps = 0
+    raise_amt = 1.04
+    
+    def __init__(self, first, last, pay):
+         
+        pass    
